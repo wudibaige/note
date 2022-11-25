adf9ad7663cb2966d94efe5a6552ea2f68fd2d68
“llf” <3279803188@qq.com>
2022-11-25 08:17:55 +0800

    上传至git

---
+++ demo-admin/admin-client/src/test/java/com/example/adminalient/AdminAlientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adminalient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminAlientApplicationTests {
+
+    @Test
+    void contextLoads( ) {
+    }
+
+}
